818c62a600c4310a98f991bcee33cf35d544782e
alu0100785265 <alu0100785265@ull.edu.es>
2017-03-25 13:45:45 +0000

    añadiendo clase medida

--- src/temperatura.js
+++ src/temperatura.js
@@ -1,3 +1,37 @@
-let ejemplo= "hola";
+class Medida{
 
-console.log(ejemplo);
+    constructor(dato){
+         var regexp = /([-+]?\d+(?:\.\d*)?)\s*((k|m|c)?m(3)?|[CcKFfk])/;
+         var m = dato.match(regexp);
+         var num = m[1];
+         var type = m[2];
+         num = parseFloat(num);
+         this.num_ = num;
+         this.type_ = type;
+    }
+    to_s(){
+        var a = this.num_.toFixed(1) + this.type_.toFixed(2);
+        console.log(a)
+        return a;
+    }
+    //Geter
+    get valor(){
+        return this.num_;
+    }
+
+    get tipo(){
+        return this.type_;
+    }
+
+    //Seter
+   set valor(value){
+        this.num=value;
+    }
+
+    set tipo(value){
+        this.type=value;
+    }
+
+
+
+}
