6a8e51644047ea8296452263309e0e206a97e798
Miguel Parra Esquivel <miguelpe83@gmail.com>
2017-03-25 15:35:08 +0000

    cambio de carpeta fichero

--- src/temperatura.js
+++ src/temperatura.js
@@ -1,3 +1,21 @@
 let ejemplo= "hola";
 
 console.log(ejemplo);
+
+
+
+class Temperatura extends Medida{
+
+        constructor(dato){
+              super(dato)
+              if ( this.type_ == "C" || this.type_ == "F" || this.type_ == "K" ||
+                  this.type_ == "c" || this.type_ == "f" || this.type_ == "k" ){
+
+                    console.log("UNIDAD DE TEMPERATURA: " + this.type_ );
+
+              }else  console.log("UNIDAD DE TEMPERATURA INCORRECTA");
+
+        }
+
+
+}
