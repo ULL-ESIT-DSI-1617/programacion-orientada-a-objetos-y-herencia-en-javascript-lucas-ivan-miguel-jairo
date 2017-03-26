3b0fb67cfca4b3f8246abe4afd458a7194d32533
alu0100785265 <alu0100785265@ull.edu.es>
2017-03-26 11:19:11 +0000

    cambios

--- src/temperatura.js
+++ src/temperatura.js
@@ -98,3 +98,34 @@ class Celsius extends Temperatura{
         }
     }
 
+ class Farenheit extends Temperatura{
+
+        constructor(dato){
+            super(dato)
+        }
+
+
+        to_c(){
+
+            var result;
+            result = (this.valor - 32)*5/9;
+            result = result.toFixed(1)+"c";
+
+            var n = new Celsius(result);
+
+            return n;
+         }
+
+
+        to_k(){
+
+            var result;
+            result = (5*(this.valor - 32)/9) + 273.15;
+            result = result.toFixed(1) +"k";
+
+            var n = new Kelvin(result);
+
+            return n;
+        }
+    }
+
